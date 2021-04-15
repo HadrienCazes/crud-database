9e3797ad9821990ce12509276c6355bd875a073f
Hadrien CAZES <hadrien.cazes@gmail.com>
2021-04-16 01:14:59 +0200

    userdb instead of user

--- create-databases.sql
+++ create-databases.sql
@@ -1,7 +1,7 @@
 create user spring_user;
 
-create database "user";
+create database userdb;
 create database events;
 create database recettes;
 
-grant all privileges on database "user",events,recettes to spring_user;
+grant all privileges on database userdb,events,recettes to spring_user;
